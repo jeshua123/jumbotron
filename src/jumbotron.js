141300f143e00cfe5fd7ccc2e66288dbcf64d76a
jeshua123 <inv.estevez@gmail.com>
2021-01-11 01:24:10 -0300

    jumbo-trom practica boostrap

---
+++ src/jumbotron.js
@@ -0,0 +1,7 @@
+export const h1= "Hello, world!"
+export const p1 = "This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information."
+export const obj1= {
+    p2:"It uses utility classes for typography and spacing to space content out within the larger container.",
+    p3:"It uses utility classes for typography and spacing to space content out within the larger container.",
+    p4a:"Learn more"
+}
---
+++ src/layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {h1,p1,obj1} from "./jumbotron"
+function Layout() {
+
+    return (
+        <>
+ 
+
+            <div class="jumbotron">
+  <h1 class="display-4">{h1}</h1>
+  <p class="lead">{p1}</p>
+  <hr class="my-4"/>
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+ </>
+    );
+}
+export default Layout;
